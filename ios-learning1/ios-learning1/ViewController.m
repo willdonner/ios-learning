75ee221cfb6c4e3ef3c21962c03d4ec73eba6fca
willdonner <765019300@qq.com>
2019-03-21 15:49:02 +0800

    TextField Button

--- ios-learning1/ios-learning1/ViewController.m
+++ ios-learning1/ios-learning1/ViewController.m
@@ -16,19 +16,24 @@
 
 @implementation ViewController
 - (IBAction)myButton:(id)sender {
-    NSLog(@"%@", _mytitle.text);
-    [_mytitle setText:@"willdoner"];
+//    [_mytitle setText:@"willdoner"];
 }
 
 - (void)viewDidLoad {
     NSLog(@"viewload");
     [super viewDidLoad];
+//    UITextField *textField1 = [[UITextField alloc]initWithFrame:CGRectMake(50, 300, 200, 50)];
+    
     // Do any additional setup after loading the view, typically from a nib.
     SampleProtocol *sampleProtocol = [[SampleProtocol alloc]init];
     sampleProtocol.delegate = self;
     NSLog(@"%@", sampleProtocol.delegate);
-    [_mytitle setText:@"Processing..."];
+    [_mytitle setText:@"##"];
+    [_mytitle setFont:[UIFont boldSystemFontOfSize:14]];
+    [_mytitle sizeToFit];
+    [mylable setText:@"Processing"];
     [sampleProtocol startSampleProcess];
+    NSLog(@"%@", mylable.text);
      [self addTextField];
 }
 
@@ -37,13 +42,42 @@
     [super didReceiveMemoryWarning];
 }
 -(void) addTextField{
+    
+    UITextField *textField1 = [[UITextField alloc]init];
+    
+    [textField1 setFrame:CGRectMake(20, 50, 280, 30)];
+    textField1.borderStyle = UITextBorderStyleRoundedRect;
+    [self.view addSubview:textField1];
+    [textField1 setFont:[UIFont boldSystemFontOfSize:14]];
+    
+    textField1.leftView =_mytitle;
+    textField1.leftViewMode = UITextFieldViewModeAlways;
+    textField1.delegate =self;
+    [textField1 setCenter:self.view.center];
+    textField1.placeholder=@"placeholder";
     NSLog(@"addTextField");
-    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(20, 50, 280, 30)];
-    [textField setText:@"2321312"];
 }
 
 #pragma mark - Sample protocol delegate
 -(void)processCompled{
-    [_mytitle setText:@"processCompled"];
+//    [_mytitle setText:@"processCompled"];
+}
+// pragma mark is used for easy access of code in Xcode
+#pragma mark - TextField Delegates
+
+// This method is called once we click inside the textField
+-(void)textFieldDidBeginEditing:(UITextField *)textField{
+    NSLog(@"Text field did begin editing");
+}
+
+// This method is called once we complete editing
+-(void)textFieldDidEndEditing:(UITextField *)textField{
+    NSLog(@"Text field ended editing");
+}
+
+// This method enables or disables the processing of return key
+-(BOOL) textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
 }
 @end
